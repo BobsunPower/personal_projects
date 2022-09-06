0c65685f4e62dba93b6d29cf77854e3a021eeaad
BobsunPower <76523077+BobsunPower@users.noreply.github.com>
2022-09-06 11:12:51 +0300

    Update rpn_calculator.py

--- rpn_calculator.py
+++ rpn_calculator.py
@@ -1,6 +1,14 @@
 # First working solution
 # Tested in PyCharm using personal laptop because of lack of administration rights to install software.
 # It doesn't recognise negative numbers.
+operators = {
+    "+": (lambda x, y: x + y),
+    "-": (lambda x, y: x - y),
+    "*": (lambda x, y: x * y),
+    "/": (lambda x, y: x / y)
+}
+
+
 def rpn_calculator(data):
     basic_math_operators = ["+", "-", "*", "/"]
     stack_of_nums = []
@@ -15,22 +23,8 @@ def rpn_calculator(data):
             exit()
     result = stack_of_nums.pop()
     while stack_of_nums and list_of_operators:
-        for index, operator in enumerate(list_of_operators):
-            if operator == "*":
-                list_of_operators.pop(index)
-                result *= stack_of_nums.pop()
-        for index, operator in enumerate(list_of_operators):
-            if operator == "/":
-                list_of_operators.pop(index)
-                result /= stack_of_nums.pop()
-        for index, operator in enumerate(list_of_operators):
-            if operator == "+":
-                list_of_operators.pop(index)
-                result += stack_of_nums.pop()
-        for index, operator in enumerate(list_of_operators):
-            if operator == "-":
-                list_of_operators.pop(index)
-                result -= stack_of_nums.pop()
+        current = result
+        result = operators[list_of_operators.pop(0)](current, stack_of_nums.pop())
     print(int(result))
 
 
